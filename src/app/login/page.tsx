d9839becd9add52421a100a1565032cabc93d59c
Henrique Barucco <henrique_barucco@hotmail.com>
2023-07-27 20:57:41 -0300

    chore: adiciona arquivos de configuração e estrutura inicial do projeto

---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+import { LoginAccount } from '@/components/login-account';
+
+export default function Login() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen py-12 bg-background">
+            <LoginAccount/>
+        </div>
+    );
+}
---
+++ src/app/register/page.tsx
@@ -0,0 +1,9 @@
+import { CreateAccount } from '@/components/create-account';
+
+export default function Register() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen py-12 bg-background">
+            <CreateAccount/>
+        </div>
+    );
+}

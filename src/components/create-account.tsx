6d50a20d0b4391adc67b73b5b6d5d240ed89993f
Henrique Barucco <henrique_barucco@hotmail.com>
2023-07-28 22:57:07 -0300

    feat(i18n.json): adiciona arquivo de tradução para suporte a múltiplos idiomas feat(en/login.json): adiciona arquivo de tradução para a página de login em inglês feat(en/register.json): adiciona arquivo de tradução para a página de registro em inglês feat(pt-br/login.json): adiciona arquivo de tradução para a página de login em português brasileiro feat(pt-br/register.json): adiciona arquivo de tradução para a página de registro em português brasileiro feat(next.config.js): adiciona suporte ao plugin next-translate feat(package.json): adiciona dependência do next-translate e next-translate-plugin fix(create-account.tsx): corrige mensagens de erro e placeholders para suporte a tradução fix(login-account.tsx): corrige mensagens de erro e placeholders para suporte a tradução

--- src/components/create-account.tsx
+++ src/components/create-account.tsx
@@ -7,24 +7,27 @@ import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
-
-const formSchema = z.object({
-    username: z
-        .string()
-        .min(2, { message: 'Username must be at least 2 characters.' })
-        .refine((val) => !/\s/.test(val), {
-            message: 'Username must not contain spaces.',
-        }),
-    password: z.string().min(6, {
-        message: 'Password must be at least 6 characters.',
-    }),
-    name: z.string().min(2, {message: 'Name must be at least 2 characters.'}),
-    email: z.string().email({
-        message: 'Please enter a valid email address.',
-    }),
-});
+import useTranslation from 'next-translate/useTranslation';
 
 export function CreateAccount() {
+    const { t } = useTranslation('register');
+
+    const formSchema = z.object({
+        username: z
+            .string()
+            .min(2, { message: t('error.username-min') })
+            .refine((val) => !/\s/.test(val), {
+                message: t('error.username-space'),
+            }),
+        password: z.string().min(6, {
+            message: t('error.password-min'),
+        }),
+        name: z.string().min(2, {message: t('error.name-min')}),
+        email: z.string().email({
+            message: t('error.email-invalid'),
+        }),
+    });
+
     const {
         handleSubmit,
         register,
@@ -34,15 +37,14 @@ export function CreateAccount() {
     });
 
     const onSubmit = (data: any) => {
-    // Handle form submission logic here
-        console.log(data);
+        data.preventDefault();
     };
 
     return (
         <Card>
             <CardHeader className="space-y-1">
-                <CardTitle className="text-2xl">Create an account</CardTitle>
-                <CardDescription>Enter your details below to create your account</CardDescription>
+                <CardTitle className="text-2xl">{t('title')}</CardTitle>
+                <CardDescription>{t('description')}</CardDescription>
             </CardHeader>
             <CardContent className="grid gap-4">
                 <div className="relative">
@@ -52,37 +54,37 @@ export function CreateAccount() {
                 </div>
                 <form onSubmit={handleSubmit(onSubmit)} className="grid gap-2">
                     <div>
-                        <Label htmlFor="username">Username</Label>
+                        <Label htmlFor="username">{t('form.username')}</Label>
                         <Input
                             id="username"
                             type="text"
-                            placeholder="Your username"
+                            placeholder={t('form.username-placeholder')}
                             {...register('username', { required: 'Username is required' })}
                         />
                         {errors.username && <span className="text-red-500">{errors.username.message?.toString()}</span>}
                     </div>
                     <div>
-                        <Label htmlFor="name">Name</Label>
+                        <Label htmlFor="name">{t('form.name')}</Label>
                         <Input
                             id="name"
                             type="text"
-                            placeholder="Your name"
+                            placeholder={t('form.name-placeholder')}
                             {...register('name', { required: 'Name is required' })}
                         />
                         {errors.name && <span className="text-red-500">{errors.name.message?.toString()}</span>}
                     </div>
                     <div>
-                        <Label htmlFor="email">Email</Label>
+                        <Label htmlFor="email">{t('form.email')}</Label>
                         <Input
                             id="email"
                             type="email"
-                            placeholder="m@example.com"
+                            placeholder={t('form.email-placeholder')}
                             {...register('email', { required: 'Email is required' })}
                         />
                         {errors.email && <span className="text-red-500">{errors.email.message?.toString()}</span>}
                     </div>
                     <div>
-                        <Label htmlFor="password">Password</Label>
+                        <Label htmlFor="password">{t('form.password')}</Label>
                         <Input
                             id="password"
                             type="password"
@@ -92,14 +94,14 @@ export function CreateAccount() {
                     </div>
                     <div>
                         <Button className="w-full" type="submit">
-                            Create account
+                            {t('form.submit')}
                         </Button>
                     </div>
                 </form>
             </CardContent>
             <div className="text-center font-light pb-3">
-                <p>Already have an account?</p>
-                <a href="/login">Login</a>
+                <p>{t('have-account')}</p>
+                <a href="/login">{t('login-account')}</a>
             </div>
         </Card>
     );
--- src/components/login-account.tsx
+++ src/components/login-account.tsx
@@ -8,15 +8,18 @@ import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
-
-const formSchema = z.object({
-    login: z.string(),
-    password: z.string().min(6, {
-        message: 'Password must be at least 6 characters.',
-    }),
-});
+import useTranslation from 'next-translate/useTranslation';
 
 export function LoginAccount() {
+    const { t } = useTranslation('login');
+    
+    const formSchema = z.object({
+        login: z.string(),
+        password: z.string().min(6, {
+            message: t('form.password-min'),
+        }),
+    });
+
     const {
         handleSubmit,
         register,
@@ -36,9 +39,9 @@ export function LoginAccount() {
     return (
         <Card>
             <CardHeader className="space-y-1">
-                <CardTitle className="text-2xl">Join into your account</CardTitle>
+                <CardTitle className="text-2xl">{t('title')}</CardTitle>
                 <CardDescription>
-                    Enter your login and password below to enter your account
+                    {t('description')}
                 </CardDescription>
             </CardHeader>
             <CardContent className="grid gap-4">
@@ -49,7 +52,7 @@ export function LoginAccount() {
                 </div>
                 <form onSubmit={handleSubmit(onSubmit)} className="grid gap-2">
                     <div>
-                        <Label htmlFor="login">Login</Label>
+                        <Label htmlFor="login">{t('form.username')}</Label>
                         <Input
                             id="login"
                             type="text"
@@ -57,7 +60,7 @@ export function LoginAccount() {
                         />
                     </div>
                     <div>
-                        <Label htmlFor="password">Password</Label>
+                        <Label htmlFor="password">{t('form.password')}</Label>
                         <Input
                             id="password"
                             type="password"
@@ -67,13 +70,13 @@ export function LoginAccount() {
                     </div>
                     <div>
                         <Button className="w-full" type="submit">
-                            Login
+                            {t('form.login')}
                         </Button>
                     </div>
                 </form>
                 <div className="text-center font-light ">
-                    <p>Dont have an account?</p>
-                    <a href="/register">Register here</a>
+                    <p>{t('no-account')}</p>
+                    <a href="/register">{t('create-account')}</a>
                 </div>
             </CardContent>
         </Card>

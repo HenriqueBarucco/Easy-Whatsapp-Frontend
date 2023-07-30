c81e1b75da71e52f69ee2b29e93612977ff1fcaa
Henrique Barucco <henrique_barucco@hotmail.com>
2023-07-29 23:58:29 -0300

    feat(locales): adiciona arquivos de tradução em inglês e português feat(components): adiciona suporte para tradução nos componentes info-account e logout

--- src/components/info-account.tsx
+++ src/components/info-account.tsx
@@ -1,13 +1,16 @@
+import useTranslation from 'next-translate/useTranslation';
 import { Separator } from './ui/separator';
 
 export function AccountInformation({ profile }: { profile: any}) {
+    const { t } = useTranslation('common');
+
     return (
         <div>
             <div className="space-y-1">
                 <h4 className="text-sm font-medium leading-none">{profile.name}</h4>
                 <Separator className="my-4" />
                 <p className="text-sm text-muted-foreground">
-                        Your instance is connected!
+                    {t('account-information.description')}
                 </p>
             </div>
         </div>
--- src/components/logout.tsx
+++ src/components/logout.tsx
@@ -1,10 +1,13 @@
 'use client';
 
+import useTranslation from 'next-translate/useTranslation';
 import { Button } from './ui/button';
 import { signOut } from 'next-auth/react';
 
 export function Logout() {
+    const { t } = useTranslation('common');
+
     return (
-        <Button onClick={() => signOut()}>Logout</Button>
+        <Button onClick={() => signOut()}>{t('default-buttons.logout')}</Button>
     );
 }

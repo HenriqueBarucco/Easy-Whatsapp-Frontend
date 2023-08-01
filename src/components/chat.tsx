8d330b5eee5884b57f80be5ab5864ddf81861e56
Henrique  Sanches (GVD) <henrique.sanches@criar.com>
2023-08-01 14:47:40 -0300

    feat(package.json): adiciona dependência "daisyui" na seção devDependencies fix(chat.tsx): remove console.log desnecessário fix(chat.tsx): atualiza a estrutura HTML e classes CSS para melhorar a exibição das mensagens de chat fix(tailwind.config.js): adiciona o plugin "daisyui" na configuração do Tailwind CSS
    A dependência "daisyui" foi adicionada como uma dependência de desenvolvimento no arquivo package.json. Isso permite que o projeto utilize recursos adicionais fornecidos pelo "daisyui" durante o desenvolvimento.
    
    O console.log desnecessário foi removido do arquivo chat.tsx para evitar poluição desnecessária no console.
    
    No arquivo chat.tsx, a estrutura HTML e as classes CSS foram atualizadas para melhorar a exibição das mensagens de chat. Isso inclui a adição de classes CSS

--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -17,6 +17,8 @@ export function Chat({ accessToken, chatMessages }: { accessToken: any, chatMess
             scrollAreaRef.current.scrollToEnd();
         }
     };
+
+    console.log(messages);
       
     return (
         <Card className="flex flex-col w-full">
@@ -29,26 +31,22 @@ export function Chat({ accessToken, chatMessages }: { accessToken: any, chatMess
                 <ScrollArea className="h-full pr-4" ref={scrollAreaRef} >
                     <div className='space-y-4'>
                         {messages?.map((message: any, index: any) => (
-                            <div
-                                key={index}
-                                className={`flex ${message.name === 'me' ? 'justify-end' : 'justify-start'}`}
-                            >
-                                <div
-                                    className={`p-2 rounded-md  ${message.name === 'me' ? 'bg-zinc-900 text-white' : 'border border-gray-800'}`}
-                                >
-                                    <div className="flex items-center">
-                                        <Avatar className="mr-4">
-                                            <AvatarImage src="https://github.com/henriquebarucco.png" alt="@henriquebarucco" />
-                                            <AvatarFallback>HB</AvatarFallback>
-                                        </Avatar>
-                                        <div className="flex flex-col max-w-3xl">
-                                            <p className="font-semibold">{message.name}</p>
-                                            <span className="whitespace-pre-wrap">{message.message}</span>
-                                        </div>
+                            <div key={index} className={`chat ${message.name === 'me' ? 'chat-end' : 'chat-start'}`}>
+                                <div className="chat-image avatar">
+                                    <div className="w-10 rounded-full">
+                                        <img src="https://github.com/henriquebarucco.png" alt={`@${message.name}`} />
                                     </div>
+                                </div>
+                                <div className="chat-header">
+                                    {message.name === 'me' ? '' : message.name}
+                                    <time className="text-xs opacity-50">{new Date(message.messageTimestamp * 1000).toLocaleTimeString()}</time>
+                                </div>
+                                <div className="chat-bubble">
+                                    {message.message}
                                 </div>
                             </div>
                         ))}
+
                     </div>
                 </ScrollArea>
             </CardContent>
